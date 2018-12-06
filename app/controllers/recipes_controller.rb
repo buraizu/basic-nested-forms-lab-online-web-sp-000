94bbf7e03efbc4e4ebf4571e456bf83e54b82536
Bryce Eadie <bryce.eadie@gmail.com>
2018-12-06 20:46:57 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,34 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new
+    end
   end
+
+  private
+
+    def recipe_params
+      params.require(:recipe).permit(:title, :ingredients_attributes => [:ingredient_1, :quantity_1, :ingredient_2, :quantity_2])
+    end
+
 end
+
+
+# {"utf8"=>"✓",
+#  "authenticity_token"=>
+#   "uN5AEtUmFp/czLXpF3HzTUvgotF9v2ltPzyEYPTLWcID/IQ9WFQpmqdHsxjQQhA6MvR252AoXnKuld/O+Y12mQ==",
+#  "recipe"=>
+#   {"title"=>"weird recipe",
+#    "ingredients"=>{"ingredient_1"=>"weed", "ingredient_2"=>"hash"}},
+#  "commit"=>"Create Recipe",
+#  "controller"=>"recipes",
+#  "action"=>"create"}
